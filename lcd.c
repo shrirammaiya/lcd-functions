cb5a4d3ce5416021004eb4446125898cbfee75a6
Shriram R Maiya <100354424+shrirammaiya@users.noreply.github.com>
2026-08-15 12:48:36 +0530

    Add .c and .h files

---
+++ lcd.c
@@ -0,0 +1,407 @@
+//////////////////////////////////////////////////////////////////////////////////	 
+//������ֻ��ѧϰʹ�ã�δ��������ɣ��������������κ���;
+//����Ӳ������Ƭ��STM32F767IGT6,����ԭ��Apollo STM32F4/F7������,��Ƶ216MHZ������12MHZ
+//QDtech-TFTҺ������ for STM32 IOģ��
+//xiao��@ShenZhen QDtech co.,LTD
+//��˾��վ:www.qdtft.com
+//�Ա���վ��http://qdtech.taobao.com
+//wiki������վ��http://www.lcdwiki.com
+//��˾�ṩ����֧�֣��κμ������⻶ӭ��ʱ����ѧϰ
+//�̻�(����) :+86 0755-23594567 
+//�ֻ�:15989313508���빤�� 
+//����:lcdwiki01@gmail.com    support@lcdwiki.com    goodtft@163.com 
+//����֧��QQ:3002773612  3002778157
+//��������QQȺ:324828016
+//��������:2018/08/22
+//�汾��V1.0
+//��Ȩ���У�����ؾ���
+//Copyright(C) ������ȫ�����Ӽ������޹�˾ 2018-2028
+//All rights reserved
+/****************************************************************************************************
+//=========================================��Դ����================================================//
+//     LCDģ��                STM32��Ƭ��
+//      VCC          ��        DC5V/3.3V      //��Դ
+//      GND          ��          GND          //��Դ��
+//=======================================Һ���������߽���==========================================//
+//��ģ��Ĭ��������������ΪSPI����
+//     LCDģ��                STM32��Ƭ��    
+//    SDI(MOSI)      ��          PB15         //Һ����SPI��������д�ź�
+//    SDO(MISO)      ��          PB14         //Һ����SPI�������ݶ��źţ��������Ҫ�������Բ�����
+//=======================================Һ���������߽���==========================================//
+//     LCDģ�� 					      STM32��Ƭ�� 
+//       LED         ��          PD6          //Һ������������źţ��������Ҫ���ƣ���5V��3.3V
+//       SCK         ��          PB13         //Һ����SPI����ʱ���ź�
+//      DC/RS        ��          PD5          //Һ��������/��������ź�
+//       RST         ��          PD12         //Һ������λ�����ź�
+//       CS          ��          PD11         //Һ����Ƭѡ�����ź�
+//=========================================������������=========================================//
+//���ģ�鲻���������ܻ��ߴ��д������ܣ����ǲ���Ҫ�������ܣ�����Ҫ���д���������
+//	   LCDģ��                STM32��Ƭ�� 
+//      T_IRQ        ��          PH11         //�����������ж��ź�
+//      T_DO         ��          PG3          //������SPI���߶��ź�
+//      T_DIN        ��          PI3          //������SPI����д�ź�
+//      T_CS         ��          PI8          //������Ƭѡ�����ź�
+//      T_CLK        ��          PH6          //������SPI����ʱ���ź�
+**************************************************************************************************/	
+ /* @attention
+  *
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, QD electronic SHALL NOT BE HELD LIABLE FOR ANY
+  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+**************************************************************************************************/
+#include "main.h"
+#include "lcd.h"
+#include "stdlib.h"
+//#include "delay.h"
+//#include "spi.h"
+extern SPI_HandleTypeDef hspi2;
+
+	   
+//����LCD��Ҫ����
+//Ĭ��Ϊ����
+_lcd_dev lcddev;
+
+//������ɫ,������ɫ
+u16 POINT_COLOR = 0x0000,BACK_COLOR = 0xFFFF;  
+u16 DeviceCode;	 
+
+/*****************************************************************************
+ * @name       :void LCD_WR_REG(u8 data)
+ * @date       :2018-08-09 
+ * @function   :Write an 8-bit command to the LCD screen
+ * @parameters :data:Command value to be written
+ * @retvalue   :None
+******************************************************************************/
+uint8_t SPI2_ReadWriteByte(uint8_t txData) {
+    uint8_t rxData = 0;
+    HAL_SPI_TransmitReceive(&hspi2, &txData, &rxData, 1, HAL_MAX_DELAY);
+//    HAL_SPI_TransmitReceive_DMA(&hspi2, &txData, &rxData, 1);
+    return rxData;
+}
+
+
+
+void LCD_WR_REG(u8 data)
+{ 
+   LCD_CS_CLR;     
+	 LCD_RS_CLR;	  
+   SPI2_ReadWriteByte(data);
+   LCD_CS_SET;	
+}
+
+/*****************************************************************************
+ * @name       :void LCD_WR_DATA(u8 data)
+ * @date       :2018-08-09 
+ * @function   :Write an 8-bit data to the LCD screen
+ * @parameters :data:data value to be written
+ * @retvalue   :None
+******************************************************************************/
+void LCD_WR_DATA(u8 data)
+{
+   LCD_CS_CLR;
+	 LCD_RS_SET;
+   SPI2_ReadWriteByte(data);
+   LCD_CS_SET;
+}
+
+/*****************************************************************************
+ * @name       :void LCD_WriteReg(u8 LCD_Reg, u16 LCD_RegValue)
+ * @date       :2018-08-09 
+ * @function   :Write data into registers
+ * @parameters :LCD_Reg:Register address
+                LCD_RegValue:Data to be written
+ * @retvalue   :None
+******************************************************************************/
+void LCD_WriteReg(u8 LCD_Reg, u16 LCD_RegValue)
+{	
+	LCD_WR_REG(LCD_Reg);  
+	LCD_WR_DATA(LCD_RegValue);	    		 
+}	   
+
+/*****************************************************************************
+ * @name       :void LCD_WriteRAM_Prepare(void)
+ * @date       :2018-08-09 
+ * @function   :Write GRAM
+ * @parameters :None
+ * @retvalue   :None
+******************************************************************************/	 
+void LCD_WriteRAM_Prepare(void)
+{
+	LCD_WR_REG(lcddev.wramcmd);
+}	 
+
+/*****************************************************************************
+ * @name       :void Lcd_WriteData_16Bit(u16 Data)
+ * @date       :2018-08-09 
+ * @function   :Write an 16-bit command to the LCD screen
+ * @parameters :Data:Data to be written
+ * @retvalue   :None
+******************************************************************************/	 
+void Lcd_WriteData_16Bit(u16 Data)
+{	
+   LCD_CS_CLR;
+   LCD_RS_SET;  
+   SPI2_ReadWriteByte(Data>>8);
+	 SPI2_ReadWriteByte(Data);
+   LCD_CS_SET;
+}
+
+/*****************************************************************************
+ * @name       :void LCD_DrawPoint(u16 x,u16 y)
+ * @date       :2018-08-09 
+ * @function   :Write a pixel data at a specified location
+ * @parameters :x:the x coordinate of the pixel
+                y:the y coordinate of the pixel
+ * @retvalue   :None
+******************************************************************************/	
+void LCD_DrawPoint(u16 x,u16 y)
+{
+	LCD_SetCursor(x,y);//���ù��λ�� 
+	Lcd_WriteData_16Bit(POINT_COLOR); 
+}
+
+/*****************************************************************************
+ * @name       :void LCD_Clear(u16 Color)
+ * @date       :2018-08-09 
+ * @function   :Full screen filled LCD screen
+ * @parameters :color:Filled color
+ * @retvalue   :None
+******************************************************************************/	
+void LCD_Clear(u16 Color)
+{
+  unsigned int i,m;  
+	LCD_SetWindows(0,0,lcddev.width-1,lcddev.height-1);   
+	LCD_CS_CLR;
+	LCD_RS_SET;
+	for(i=0;i<lcddev.height;i++)
+	{
+    for(m=0;m<lcddev.width;m++)
+    {	
+		SPI2_ReadWriteByte(Color>>8);
+	 	SPI2_ReadWriteByte(Color);
+		}
+	}
+	 LCD_CS_SET;
+} 
+
+/*****************************************************************************
+ * @name       :void LCD_Clear(u16 Color)
+ * @date       :2018-08-09 
+ * @function   :Initialization LCD screen GPIO
+ * @parameters :None
+ * @retvalue   :None
+******************************************************************************/	
+void LCD_GPIOInit(void)
+{
+//	GPIO_InitTypeDef GPIO_Initure;
+//
+//    __HAL_RCC_GPIOD_CLK_ENABLE();           //ʹ��GPIOFʱ��
+//
+//    //PF6
+//    GPIO_Initure.Pin=GPIO_PIN_5| GPIO_PIN_6|GPIO_PIN_11| GPIO_PIN_12;            //PF6
+//    GPIO_Initure.Mode=GPIO_MODE_OUTPUT_PP;  //�������
+//    GPIO_Initure.Pull=GPIO_PULLUP;          //����
+//    GPIO_Initure.Speed=GPIO_SPEED_HIGH;     //����
+//    HAL_GPIO_Init(GPIOD,&GPIO_Initure);     //��ʼ��
+//		HAL_GPIO_WritePin(GPIOD,GPIO_PIN_5| GPIO_PIN_6|GPIO_PIN_11|GPIO_PIN_12,GPIO_PIN_SET);
+}
+
+/*****************************************************************************
+ * @name       :void LCD_RESET(void)
+ * @date       :2018-08-09 
+ * @function   :Reset LCD screen
+ * @parameters :None
+ * @retvalue   :None
+******************************************************************************/	
+void LCD_RESET(void)
+{
+	LCD_RST_CLR;
+	HAL_Delay(100);
+	LCD_RST_SET;
+	HAL_Delay(50);
+}
+
+/*****************************************************************************
+ * @name       :void LCD_RESET(void)
+ * @date       :2018-08-09 
+ * @function   :Initialization LCD screen
+ * @parameters :None
+ * @retvalue   :None
+******************************************************************************/	 	 
+void LCD_Init(void)
+{  
+//	SPI2_Init();		   			        //��ʼ��SPI
+//	SPI2_SetSpeed(SPI_BAUDRATEPRESCALER_2); //����Ϊ45Mʱ��,����ģʽ
+	LCD_GPIOInit();//LCD GPIO��ʼ��										 
+ 	LCD_RESET(); //LCD ��λ
+//************* ST7796S��ʼ��**********//	
+	LCD_WR_REG(0xF0);
+	LCD_WR_DATA(0xC3);
+	LCD_WR_REG(0xF0);
+	LCD_WR_DATA(0x96);
+	LCD_WR_REG(0x36);
+	LCD_WR_DATA(0x68);	
+	LCD_WR_REG(0x3A);
+	LCD_WR_DATA(0x05);	
+	LCD_WR_REG(0xB0);
+	LCD_WR_DATA(0x80);	
+	LCD_WR_REG(0xB6);
+	LCD_WR_DATA(0x00);
+	LCD_WR_DATA(0x02);	
+	LCD_WR_REG(0xB5);
+	LCD_WR_DATA(0x02);
+	LCD_WR_DATA(0x03);
+	LCD_WR_DATA(0x00);
+	LCD_WR_DATA(0x04);
+	LCD_WR_REG(0xB1);
+	LCD_WR_DATA(0x80);	
+	LCD_WR_DATA(0x10);	
+	LCD_WR_REG(0xB4);
+	LCD_WR_DATA(0x00);
+	LCD_WR_REG(0xB7);
+	LCD_WR_DATA(0xC6);
+	LCD_WR_REG(0xC5);
+	LCD_WR_DATA(0x24);
+	LCD_WR_REG(0xE4);
+	LCD_WR_DATA(0x31);
+	LCD_WR_REG(0xE8);
+	LCD_WR_DATA(0x40);
+	LCD_WR_DATA(0x8A);
+	LCD_WR_DATA(0x00);
+	LCD_WR_DATA(0x00);
+	LCD_WR_DATA(0x29);
+	LCD_WR_DATA(0x19);
+	LCD_WR_DATA(0xA5);
+	LCD_WR_DATA(0x33);
+	LCD_WR_REG(0xC2);
+	LCD_WR_REG(0xA7);
+	
+	LCD_WR_REG(0xE0);
+	LCD_WR_DATA(0xF0);
+	LCD_WR_DATA(0x09);
+	LCD_WR_DATA(0x13);
+	LCD_WR_DATA(0x12);
+	LCD_WR_DATA(0x12);
+	LCD_WR_DATA(0x2B);
+	LCD_WR_DATA(0x3C);
+	LCD_WR_DATA(0x44);
+	LCD_WR_DATA(0x4B);
+	LCD_WR_DATA(0x1B);
+	LCD_WR_DATA(0x18);
+	LCD_WR_DATA(0x17);
+	LCD_WR_DATA(0x1D);
+	LCD_WR_DATA(0x21);
+
+	LCD_WR_REG(0XE1);
+	LCD_WR_DATA(0xF0);
+	LCD_WR_DATA(0x09);
+	LCD_WR_DATA(0x13);
+	LCD_WR_DATA(0x0C);
+	LCD_WR_DATA(0x0D);
+	LCD_WR_DATA(0x27);
+	LCD_WR_DATA(0x3B);
+	LCD_WR_DATA(0x44);
+	LCD_WR_DATA(0x4D);
+	LCD_WR_DATA(0x0B);
+	LCD_WR_DATA(0x17);
+	LCD_WR_DATA(0x17);
+	LCD_WR_DATA(0x1D);
+	LCD_WR_DATA(0x21);
+
+  LCD_WR_REG(0X36);
+	LCD_WR_DATA(0xEC);
+	LCD_WR_REG(0xF0);
+	LCD_WR_DATA(0xC3);
+	LCD_WR_REG(0xF0);
+	LCD_WR_DATA(0x69);
+	LCD_WR_REG(0X13);
+	LCD_WR_REG(0X11);
+	LCD_WR_REG(0X29);
+	
+  LCD_direction(USE_HORIZONTAL);//����LCD��ʾ����
+	LCD_LED(1);//��������	 
+	LCD_Clear(WHITE);//��ȫ����ɫ
+}
+ 
+/*****************************************************************************
+ * @name       :void LCD_SetWindows(u16 xStar, u16 yStar,u16 xEnd,u16 yEnd)
+ * @date       :2018-08-09 
+ * @function   :Setting LCD display window
+ * @parameters :xStar:the bebinning x coordinate of the LCD display window
+								yStar:the bebinning y coordinate of the LCD display window
+								xEnd:the endning x coordinate of the LCD display window
+								yEnd:the endning y coordinate of the LCD display window
+ * @retvalue   :None
+******************************************************************************/ 
+void LCD_SetWindows(u16 xStar, u16 yStar,u16 xEnd,u16 yEnd)
+{	
+	LCD_WR_REG(lcddev.setxcmd);	
+	LCD_WR_DATA(xStar>>8);
+	LCD_WR_DATA(0x00FF&xStar);		
+	LCD_WR_DATA(xEnd>>8);
+	LCD_WR_DATA(0x00FF&xEnd);
+
+	LCD_WR_REG(lcddev.setycmd);	
+	LCD_WR_DATA(yStar>>8);
+	LCD_WR_DATA(0x00FF&yStar);		
+	LCD_WR_DATA(yEnd>>8);
+	LCD_WR_DATA(0x00FF&yEnd);
+
+	LCD_WriteRAM_Prepare();	//��ʼд��GRAM			
+}   
+
+/*****************************************************************************
+ * @name       :void LCD_SetCursor(u16 Xpos, u16 Ypos)
+ * @date       :2018-08-09 
+ * @function   :Set coordinate value
+ * @parameters :Xpos:the  x coordinate of the pixel
+								Ypos:the  y coordinate of the pixel
+ * @retvalue   :None
+******************************************************************************/ 
+void LCD_SetCursor(u16 Xpos, u16 Ypos)
+{	  	    			
+	LCD_SetWindows(Xpos,Ypos,Xpos,Ypos);	
+} 
+
+/*****************************************************************************
+ * @name       :void LCD_direction(u8 direction)
+ * @date       :2018-08-09 
+ * @function   :Setting the display direction of LCD screen
+ * @parameters :direction:0-0 degree
+                          1-90 degree
+													2-180 degree
+													3-270 degree
+ * @retvalue   :None
+******************************************************************************/ 
+void LCD_direction(u8 direction)
+{ 
+	lcddev.setxcmd=0x2A;
+	lcddev.setycmd=0x2B;
+	lcddev.wramcmd=0x2C;
+	switch(direction){		  
+		case 0:						 	 		
+			lcddev.width=LCD_W;
+			lcddev.height=LCD_H;		
+			LCD_WriteReg(0x36,(1<<3)|(1<<6));
+		break;
+		case 1:
+			lcddev.width=LCD_H;
+			lcddev.height=LCD_W;
+			LCD_WriteReg(0x36,(1<<3)|(1<<5));
+		break;
+		case 2:						 	 		
+			lcddev.width=LCD_W;
+			lcddev.height=LCD_H;	
+			LCD_WriteReg(0x36,(1<<3)|(1<<7));
+		break;
+		case 3:
+			lcddev.width=LCD_H;
+			lcddev.height=LCD_W;
+			LCD_WriteReg(0x36,(1<<3)|(1<<7)|(1<<6)|(1<<5));
+		break;	
+		default:break;
+	}		
+}	 
---
+++ lcd.h
@@ -0,0 +1,180 @@
+//////////////////////////////////////////////////////////////////////////////////	 
+//������ֻ��ѧϰʹ�ã�δ��������ɣ��������������κ���;
+//����Ӳ������Ƭ��STM32F767IGT6,����ԭ��Apollo STM32F4/F7������,��Ƶ216MHZ������12MHZ
+//QDtech-TFTҺ������ for STM32 IOģ��
+//xiao��@ShenZhen QDtech co.,LTD
+//��˾��վ:www.qdtft.com
+//�Ա���վ��http://qdtech.taobao.com
+//wiki������վ��http://www.lcdwiki.com
+//��˾�ṩ����֧�֣��κμ������⻶ӭ��ʱ����ѧϰ
+//�̻�(����) :+86 0755-23594567 
+//�ֻ�:15989313508���빤�� 
+//����:lcdwiki01@gmail.com    support@lcdwiki.com    goodtft@163.com 
+//����֧��QQ:3002773612  3002778157
+//��������QQȺ:324828016
+//��������:2018/08/22
+//�汾��V1.0
+//��Ȩ���У�����ؾ���
+//Copyright(C) ������ȫ�����Ӽ������޹�˾ 2018-2028
+//All rights reserved
+/****************************************************************************************************
+//=========================================��Դ����================================================//
+//     LCDģ��                STM32��Ƭ��
+//      VCC          ��        DC5V/3.3V      //��Դ
+//      GND          ��          GND          //��Դ��
+//=======================================Һ���������߽���==========================================//
+//��ģ��Ĭ��������������ΪSPI����
+//     LCDģ��                STM32��Ƭ��    
+//    SDI(MOSI)      ��          PB15         //Һ����SPI��������д�ź�
+//    SDO(MISO)      ��          PB14         //Һ����SPI�������ݶ��źţ��������Ҫ�������Բ�����
+//=======================================Һ���������߽���==========================================//
+//     LCDģ�� 					      STM32��Ƭ�� 
+//       LED         ��          PD6          //Һ������������źţ��������Ҫ���ƣ���5V��3.3V
+//       SCK         ��          PB13         //Һ����SPI����ʱ���ź�
+//      DC/RS        ��          PD5          //Һ��������/��������ź�
+//       RST         ��          PD12         //Һ������λ�����ź�
+//       CS          ��          PD11         //Һ����Ƭѡ�����ź�
+//=========================================������������=========================================//
+//���ģ�鲻���������ܻ��ߴ��д������ܣ����ǲ���Ҫ�������ܣ�����Ҫ���д���������
+//	   LCDģ��                STM32��Ƭ�� 
+//      T_IRQ        ��          PH11         //�����������ж��ź�
+//      T_DO         ��          PG3          //������SPI���߶��ź�
+//      T_DIN        ��          PI3          //������SPI����д�ź�
+//      T_CS         ��          PI8          //������Ƭѡ�����ź�
+//      T_CLK        ��          PH6          //������SPI����ʱ���ź�
+**************************************************************************************************/	
+ /* @attention
+  *
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, QD electronic SHALL NOT BE HELD LIABLE FOR ANY
+  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+**************************************************************************************************/
+	
+#ifndef __LCD_H
+#define __LCD_H		
+#include "stm32f4xx_hal.h"
+#include "stdlib.h"
+#include "main.h"
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+
+typedef int8_t   s8;
+typedef int16_t  s16;
+typedef int32_t  s32;
+
+
+//LCD��Ҫ������
+typedef struct  
+{										    
+	u16 width;			//LCD ���
+	u16 height;			//LCD �߶�
+	u16 id;				//LCD ID
+	u8  dir;			//���������������ƣ�0��������1��������	
+	u16	 wramcmd;		//��ʼдgramָ��
+	u16  setxcmd;		//����x����ָ��
+	u16  setycmd;		//����y����ָ��	
+}_lcd_dev; 	
+
+//LCD����
+extern _lcd_dev lcddev;	//����LCD��Ҫ����
+/////////////////////////////////////�û�������///////////////////////////////////	 
+#define USE_HORIZONTAL  	 0//����Һ����˳ʱ����ת���� 	0-0����ת��1-90����ת��2-180����ת��3-270����ת
+
+//////////////////////////////////////////////////////////////////////////////////	  
+//����LCD�ĳߴ�
+#define LCD_W 320
+#define LCD_H 480
+
+//TFTLCD������Ҫ���õĺ���		   
+extern u16  POINT_COLOR;//Ĭ�Ϻ�ɫ    
+extern u16  BACK_COLOR; //������ɫ.Ĭ��Ϊ��ɫ
+
+////////////////////////////////////////////////////////////////////
+//-----------------LCD�˿ڶ���---------------- 
+//QDtechȫϵ��ģ������������ܿ��Ʊ��������û�Ҳ���Խ�PWM���ڱ�������
+#define	LCD_LED(n) (n?HAL_GPIO_WritePin(TFT_LED_GPIO_Port,TFT_LED_Pin,GPIO_PIN_SET):HAL_GPIO_WritePin(TFT_LED_GPIO_Port,TFT_LED_Pin,GPIO_PIN_RESET)) //LCD����
+#define LCD_CS(n)  (n?HAL_GPIO_WritePin(TFT_CS_GPIO_Port,TFT_CS_Pin,GPIO_PIN_SET):HAL_GPIO_WritePin(TFT_CS_GPIO_Port,TFT_CS_Pin,GPIO_PIN_RESET))
+#define LCD_RS(n)  (n?HAL_GPIO_WritePin(TFT_DC_GPIO_Port,TFT_DC_Pin,GPIO_PIN_SET):HAL_GPIO_WritePin(TFT_DC_GPIO_Port,TFT_DC_Pin,GPIO_PIN_RESET))
+#define LCD_RST(n) (n?HAL_GPIO_WritePin(TFT_RST_GPIO_Port,TFT_RST_Pin,GPIO_PIN_SET):HAL_GPIO_WritePin(TFT_RST_GPIO_Port,TFT_RST_Pin,GPIO_PIN_RESET))
+//���ʹ�ùٷ��⺯���������еײ㣬�ٶȽ����½���14֡ÿ�룬���������˾�Ƽ�����
+//����IO����ֱ�Ӳ����Ĵ���������IO������ˢ�����ʿ��Դﵽ28֡ÿ�룡 
+
+#define	LCD_CS_SET  LCD_CS(1)//GPIO_TYPE->BSRR=1<<LCD_CS    //Ƭѡ�˿�  	PB11
+#define	LCD_RS_SET	LCD_RS(1)//GPIO_TYPE->BSRR=1<<LCD_RS    //����/����  PB10	  
+#define	LCD_RST_SET	LCD_RST(1)//GPIO_TYPE->BSRR=1<<LCD_RST    //��λ			PB12
+
+ 							    
+#define	LCD_CS_CLR  LCD_CS(0)//GPIO_TYPE->BRR=1<<LCD_CS     //Ƭѡ�˿�  	PB11
+#define	LCD_RS_CLR	LCD_RS(0)//GPIO_TYPE->BRR=1<<LCD_RS     //����/����  PB10	 
+#define	LCD_RST_CLR	LCD_RST(0)//GPIO_TYPE->BRR=1<<LCD_RST    //��λ			  PB12
+
+				
+
+
+//������ɫ
+#define WHITE       0xFFFF
+#define BLACK      	0x0000	  
+#define BLUE       	0x001F  
+#define BRED        0XF81F
+#define GRED 			 	0XFFE0
+#define GBLUE			 	0X07FF
+#define RED         0xF800
+#define MAGENTA     0xF81F
+#define GREEN       0x07E0
+#define CYAN        0x7FFF
+#define YELLOW      0xFFE0
+#define BROWN 			0XBC40 //��ɫ
+#define BRRED 			0XFC07 //�غ�ɫ
+#define GRAY  			0X8430 //��ɫ
+#define GRAY0       0xEF7D 
+#define GRAY1       0x8410      	//��ɫ1      00000 000000 00000
+#define GRAY2       0x4208 
+//GUI��ɫ
+
+#define DARKBLUE      	 0X01CF	//����ɫ
+#define LIGHTBLUE      	 0X7D7C	//ǳ��ɫ  
+#define GRAYBLUE       	 0X5458 //����ɫ
+//������ɫΪPANEL����ɫ 
+ 
+#define LIGHTGREEN     	0X841F //ǳ��ɫ
+#define LIGHTGRAY     0XEF5B //ǳ��ɫ(PANNEL)
+#define LGRAY 			 		0XC618 //ǳ��ɫ(PANNEL),���屳��ɫ
+
+#define LGRAYBLUE      	0XA651 //ǳ����ɫ(�м����ɫ)
+#define LBBLUE          0X2B12 //ǳ����ɫ(ѡ����Ŀ�ķ�ɫ)
+	    															  
+void LCD_Init(void);
+void LCD_DisplayOn(void);
+void LCD_DisplayOff(void);
+void LCD_Clear(u16 Color);	 
+void LCD_SetCursor(u16 Xpos, u16 Ypos);
+void LCD_DrawPoint(u16 x,u16 y);//����
+u16  LCD_ReadPoint(u16 x,u16 y); //����
+void LCD_DrawLine(u16 x1, u16 y1, u16 x2, u16 y2);
+void LCD_DrawRectangle(u16 x1, u16 y1, u16 x2, u16 y2);		   
+void LCD_SetWindows(u16 xStar, u16 yStar,u16 xEnd,u16 yEnd);
+
+u16 LCD_RD_DATA(void);//��ȡLCD����									    
+void LCD_WriteReg(u8 LCD_Reg, u16 LCD_RegValue);
+void LCD_WR_DATA(u8 data);
+u16 LCD_ReadReg(u8 LCD_Reg);
+void LCD_WriteRAM_Prepare(void);
+void LCD_WriteRAM(u16 RGB_Code);
+u16 LCD_ReadRAM(void);		   
+u16 LCD_BGR2RGB(u16 c);
+void LCD_SetParam(void);
+void Lcd_WriteData_16Bit(u16 Data);
+void LCD_direction(u8 direction );
+
+
+
+#endif  
+	 
+	 
+
+
+
